34d9ba265238a731139f9efb48572e7d5b593fa4
Narendra Kumar Vadapalli <nvadapalli@spirestudios.com>
2025-01-28 22:17:11 -0800

    feat(adding-vue-router): Adding router and views that use components

---
+++ src/components/Welcome.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+
+  <div>
+    <h1 class="p-8">{{ msg }}</h1>
+  </div>
+
+  <div class="p-8">
+    <button class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded" type="button"
+      @click="count++">count is {{ count }}
+    </button>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+
+    <h1 class="p-8">Home</h1>
+
+    <Welcome msg="Naren Vue3 + Vue + TS 2025 Starter" />
+
+    <button @click="goToAbout">Go to About</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Welcome from '../components/Welcome.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Welcome
+  },
+  methods: {
+    goToAbout() {
+      this.$router.push('/about');
+    }
+  }
+});
+</script>
